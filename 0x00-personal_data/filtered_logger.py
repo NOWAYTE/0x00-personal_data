25883d03937dc1da07d8f164d6b977051550e627
Daniel kamau <danielkamaunganga3@gmail.com>
2024-11-10 15:24:15 +0300

    Update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,11 +5,13 @@ import logging
 import re
 
 
-def filter_datum(fields: list[str], redaction: str, message: str,separator: str) -> str:
+def filter_datum(fields: list[str], redaction: str, message: str,
+                 separator: str) -> str:
     """Replaces values with specified fields"""
     for i in fields:
-        print(i)
-        pattern = re.sub(rf"{i}=(.*?)(?={separator}|$)",
-               f'{i}={redaction}', message)
-
+        pattern = re.sub(
+            rf"{i}=(.*?)(?={separator}|$)",
+            f"{i}={redaction}",
+            message
+        )
     return pattern
