ee0bd0ad7dd4f8cc08362cb24ee7bf66aab07fa9
Daniel kamau <danielkamaunganga3@gmail.com>
2024-11-10 16:24:58 +0300

    Fix code error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,16 +8,6 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """Replacing"""
-    if not (
-            isinstance(fields, List) and
-            all(isinstance(item, str) for item in fields)):
-        raise TypeError("Fields must be a list of strings")
-    if not isinstance(redaction, str):
-        raise TypeError("Redaction must be a string")
-    if not isinstance(message, str):
-        raise TypeError("Message must be a string")
-    if not isinstance(separator, str):
-        raise TypeError("Separator must be a string")
 
     for field in fields:
         pattern = re.compile(rf"{field}=(.*?){separator}")
