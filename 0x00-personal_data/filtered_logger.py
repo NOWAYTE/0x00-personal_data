b00bb73d87f90ff786587a3536c5ebc18e563d7e
Daniel kamau <danielkamaunganga3@gmail.com>
2024-11-10 16:22:43 +0300

    Fix code error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,8 @@ import re
 from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """Replacing"""
     if not (
             isinstance(fields, List) and
