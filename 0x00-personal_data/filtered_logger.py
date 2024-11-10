5694d96d6168a624f17d8e6a3bb425d674ddea67
Daniel kamau <danielkamaunganga3@gmail.com>
2024-11-10 16:26:58 +0300

    Fix code error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,7 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
+def filter_datum(fields: list[str], redaction: str, message: str,
                  separator: str) -> str:
     """Replacing"""
 
