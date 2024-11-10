6daa39771137bd6980f52e90ba7dd26893955dbb
Daniel kamau <danielkamaunganga3@gmail.com>
2024-11-10 16:11:54 +0300

    Update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
 """Regex-ing"""
 
-import logging
 import re
+from typing import List
 
 
-def filter_datum(fields: list[str], redaction: str, message: str,
+def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """ Replacing """
-    for f in fields:
-        message = re.sub(rf"{f}=(.*?)\{separator}",
-                         f'{f}={redaction}{separator}', message)
+    """Replacing"""
+    if not (
+            isinstance(fields, list) and
+            all(isinstance(item, str) for item in fields)):
+        raise TypeError("Fields must be a list of strings")
+    if not isinstance(redaction, str):
+        raise TypeError("Redaction must be a string")
+    if not isinstance(message, str):
+        raise TypeError("Message must be a string")
+    if not isinstance(separator, str):
+        raise TypeError("Separator must be a string")
+
+    for field in fields:
+        pattern = re.compile(rf"{field}=(.*?){separator}")
+        message = pattern.sub(f"{field}={redaction}{separator}", message)
+
     return message
