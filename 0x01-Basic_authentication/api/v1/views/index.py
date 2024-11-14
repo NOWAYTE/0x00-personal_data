6d9ab07e8c19c6d7e536af5bda2c982333466fdf
Daniel kamau <danielkamaunganga3@gmail.com>
2024-11-14 20:48:43 +0300

    Forbidden error handler response

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -22,6 +22,14 @@ def unauthorized() -> str:
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - Forbidden error response
+    """
+
+    return jsonify({"error": "Forbidden"}), 403
 
 @app_views.route('/stats/', methods=['GET'], strict_slashes=False)
 def stats() -> str:
