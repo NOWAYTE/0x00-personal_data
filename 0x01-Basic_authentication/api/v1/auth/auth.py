9da87e44a1623ec92b57ea7a5ff1b9d102e6d0b7
Daniel kamau <danielkamaunganga3@gmail.com>
2024-11-15 16:45:03 +0300

    Route that don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,8 +13,19 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Checks path
         """
+        if not path:
+            return True
+        if not excluded_paths:
+            return True
+        path = path if path.endswith('/') else path + '/'
 
-        return False
+        for e_path in excluded_paths:
+            e_path = e_path if e_path.endswith('/') else e_path + '/'
+
+            if path == excluded_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Returns authorization header value
